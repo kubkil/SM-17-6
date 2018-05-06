3ee52023a86971620d82c926d1c716adc656be7c
Jakub Kilar <kuba.kilar@gmail.com>
2018-05-06 16:35:45 +0200

    add logged page after lig clclicked

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
   res.render('log');
 });
 
+// OTWORZENIE STRONY PO KLIKNIĘCIU NA LOG W PUG
+app.get('/auth/google', (req, res) => {
+  res.render('logged');
+});
+
 app.listen(3000);
 app.use((req, res, next) => {
   res.status(404).send('Page not found');
